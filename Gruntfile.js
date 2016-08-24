17cdec9f186d470dc30b87021d1391da8dadb1c0
mzymta <ekonyakhin@gmail.com>
2016-08-24 16:13:51 +0300

    added grunt

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+
+    // 1. Вся настройка находится здесь
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'js/libs/*.js',
+                    'js/script.js'
+                ],
+                dest: 'js/build/production.js'
+            }
+        },
+        uglify: {
+            build: {
+                src: 'js/build/production.js',
+                dest: 'js/build/production.min.js'
+            }
+        }
+
+    });
+
+    // 3. Тут мы указываем Grunt, что хотим использовать этот плагин
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
+    grunt.registerTask('default', ['concat', 'uglify']);
+
+};
